ee3bf728799b47ee43cfd240dc931421b966c4e6
Dariusz Filipiak <dariusz.filipiak@codete.com>
2022-02-20 16:14:58 +0100

    blury background

--- apps/codete/src/app/app.module.ts
+++ apps/codete/src/app/app.module.ts
@@ -4,11 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+const materialModules = [
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule,
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ...materialModules,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
