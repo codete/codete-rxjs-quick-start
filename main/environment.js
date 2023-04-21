a12ba4c138298daf143bbf86889a1732aa8715b4
Dariusz Filipiak <dariusz.filipiak@codete.com>
2023-04-21 05:33:26 +0200

    pwa fixes

--- main/environment.js
+++ main/environment.js
@@ -5,7 +5,11 @@ var { config } = { config: {} };
 config = {
 
     domain: 'main.example.domain.com',
-    title: 'Codete RxJs Quick Start'
+    title: 'Codete RxJs Quick Start',
+    pwa: {
+      name: 'Codete RxJs Quick Start',
+      short_name: 'codete-rxjs-start',
+    }
 
 
 }
