5998e55cc44ea65d2141844cfd298df03f03a59d
Dariusz Filipiak <dariusz.filipiak@codete.com>
2023-05-27 21:37:17 +0200

    update

--- main/environment.js
+++ main/environment.js
@@ -3,13 +3,34 @@ const path = require('path')
 var { config } = { config: {} };
 
 config = {
-
-    domain: 'main.example.domain.com',
-    title: 'Codete RxJs Quick Start',
-    pwa: {
-      name: 'Codete RxJs Quick Start',
-      short_name: 'codete-rxjs-start',
+  plugins: {
+    'ngx-progressbar': {
+      spinnerPosition: 'left'
     }
+  },
+  loading: {
+    preAngularBootstrap: {
+      loader: 'main/src/assets/logo2-low-opacity.svg',
+      background: '#9395ab',
+    },
+    afterAngularBootstrap: {
+      loader: {
+        name: 'lds-heart',
+        color: '#c85958'
+      },
+      // loader: {
+      //   name: 'lds-default',
+      //   color: '#c85958'
+      // },
+      background: '#9395abb5',
+    },
+  },
+  domain: 'main.example.domain.com',
+  title: 'Codete RxJs Quick Start',
+  pwa: {
+    name: 'Codete RxJs Quick Start',
+    short_name: 'codete-rxjs-start',
+  }
 
 
 }
