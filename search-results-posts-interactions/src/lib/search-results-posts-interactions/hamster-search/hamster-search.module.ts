6b411fc6d33cb0ba49d3a6d8cb8bcbcef7025c03
Dariusz Filipiak <darekf77@gmail.com>
2023-06-09 20:08:40 +0200

    angular 16 update

--- search-results-posts-interactions/src/lib/search-results-posts-interactions/hamster-search/hamster-search.module.ts
+++ search-results-posts-interactions/src/lib/search-results-posts-interactions/hamster-search/hamster-search.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HamsterSearchComponent } from './hamster-search.component';
 import { HamsterModule, MaterialModule } from '@codete-rxjs-quick-start/shared';
-
+import { StaticColumnsModule } from 'static-columns';
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     HamsterModule,
+    StaticColumnsModule,
   ],
   declarations: [HamsterSearchComponent],
   exports: [HamsterSearchComponent],
